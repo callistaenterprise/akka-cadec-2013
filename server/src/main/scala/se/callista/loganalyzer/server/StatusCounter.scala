3d3f0ce75a416458e6a59b8f1bf4649197c781d2
Albert Örwall <hvnter@gmail.com>
2013-01-09 14:50:46 +0100

    fixed idempotent solution in counter

--- server/src/main/scala/se/callista/loganalyzer/server/StatusCounter.scala
+++ server/src/main/scala/se/callista/loganalyzer/server/StatusCounter.scala
@@ -16,14 +16,31 @@ import se.callista.loganalyzer.{Count, HttpStatus, LogMessage}
  */
 class StatusCounter(status: HttpStatus, presenter: ActorRef) extends Actor with ActorLogging {
 
+  /* Simple solution: */
+//  var count = 0
+//  
+//  def receive = {
+//    case LogMessage(host, id, log) => {
+//      count = count + 1
+//      val c = new Count(status, count)
+//      presenter ! c
+//    }
+//  }
+//  
+  
+  // Idempotent solution:
+  val ids = HashSet[String]()
   var count = 0
   
   def receive = {
     case LogMessage(host, id, log) => {
-      count = count + 1
-      val c = new Count(status, count)
-      presenter ! c
+      if(!ids.contains(host + ":" + id)){
+        ids += host + ":" + id
+        count = count + 1
+        val c = new Count(status, count)
+        presenter ! c
+      }
+      
     }
   }
-  
 }
