6b1998e0d8f350503b4069cc70e34fa1f8fb288d
Albert Örwall <hvnter@gmail.com>
2013-01-14 09:41:35 +0100

    more counter tests

--- server/src/test/scala/se/callista/loganalyzer/server/StatusCounterSuite.scala
+++ server/src/test/scala/se/callista/loganalyzer/server/StatusCounterSuite.scala
@@ -8,8 +8,9 @@ import com.typesafe.config.ConfigFactory
 import org.junit.runner.RunWith
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import org.scalatest.junit.JUnitRunner
-import se.callista.loganalyzer.{Success, AccessLog, LogMessage, Count}
+import se.callista.loganalyzer._
 import java.util.Date
+import akka.testkit.TestActorRef
 
 @RunWith(classOf[JUnitRunner])
 class StatusCounterSuite(_system: ActorSystem) extends TestKit(_system) 
@@ -21,16 +22,35 @@ class StatusCounterSuite(_system: ActorSystem) extends TestKit(_system)
     system.shutdown()
   }
   
-  test("count logs and send value to presenter when a log message arrives") {
+  test("StatusCounter should count logs and send value to presenter when a log message arrives") {
     val accessLog = AccessLog("127.0.0.1", new Date, "GET", "/", Random.nextInt(400)+200, 10)
     
     val probe = TestProbe()
-    val actor = system.actorOf(Props(new StatusCounter(Success, probe.ref)))
+    val actor = TestActorRef(new StatusCounter(Success, probe.ref))
     
     actor ! LogMessage("hostname", 1, accessLog)
     probe.expectMsg(1 second, Count(Success, 1)) 
     actor ! LogMessage("hostname", 2, accessLog)
     probe.expectMsg(1 second, Count(Success, 2))
   }
-
+  
+  val server = TestActorRef(new LogServer(testActor))
+  
+  test("LogServer should route successful (status < 400) requests") {
+    val successLog = AccessLog("127.0.0.1", new Date, "GET", "/", 200, 10)   
+    server ! LogMessage("hostname", 1, successLog)
+    expectMsg(Count(Success, 1))
+  }
+  
+  test("LogServer should route client errors (status >= 400 & < 500)") {
+    val clientError = AccessLog("127.0.0.1", new Date, "GET", "/", 404, 10)   
+    server ! LogMessage("hostname", 1, clientError)
+    expectMsg(Count(ClientError, 1))
+  }
+  
+  test("LogServer should route server errors (status >= 500)") {
+    val serverError = AccessLog("127.0.0.1", new Date, "GET", "/", 503, 10)   
+    server ! LogMessage("hostname", 1, serverError)
+    expectMsg(Count(ServerError, 1))    
+  }
 }
